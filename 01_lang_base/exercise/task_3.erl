d26a43aab229e000d4592df6d4da269088fa4b02
Victor Buryak <vicctor.b.0711@gmail.com>
2019-03-25 11:36:16 +0300

    task 3 completed

--- 01_lang_base/exercise/task_3.erl
+++ 01_lang_base/exercise/task_3.erl
@@ -8,7 +8,9 @@
 %% Calculate distance between 2 points on 2D surface
 %% points are given as tuple {X, Y}, distance should be float
 distance({X1, Y1}, {X2, Y2}) ->
-    0.0.
+    DeltaX = abs(X1-X2),
+    DeltaY = abs(Y1-Y2),
+    CalculatedDistanse = math:sqrt(math:pow(DeltaX,2) + math:pow(DeltaY,2)).
 
 distance_test() ->
     ?assertEqual(5.0, distance({0, 0}, {0, 5})),
