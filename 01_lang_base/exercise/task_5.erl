71db41b11aa013d32f537ef3de4896769db9b73b
Victor Buryak <vicctor.b.0711@gmail.com>
2019-03-25 12:00:39 +0300

    task 5 completed and all tests were succesfully passed

--- 01_lang_base/exercise/task_5.erl
+++ 01_lang_base/exercise/task_5.erl
@@ -7,7 +7,8 @@
 
 %% get 3 float numbers from binary
 get_floats(Bin) ->
-    {0.0, 0.0, 0.0}.
+    <<F1/float, F2/float, F3/float>> = Bin,
+    {F1,F2,F3}.
 
 get_floats_test() ->
     ?assertEqual({1.0, 2.5, 100.500},
