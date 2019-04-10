624e088fc9fca00811cc85103dfda0cfaa86eca8
Victor Buryak <vicctor.b.0711@gmail.com>
2019-04-10 09:16:35 +0300

    init method for task of lesson 07

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -1,6 +1,17 @@
 -module(template).
 
--export([parse/2]).
+-export([parse/2, init/0]).
+
+
+init() -> 
+	In = <<"User {{name}} won {{wins}} games and got {{points}} points">>,
+	Data = #{<<"name">> => "Kate",
+             <<"wins">> => 55,
+             <<"points">> => 777},
+    {In, Data}.
 
 parse(Str, Data) when is_binary(Str) ->
-    Str.
+	Keys = maps:fold(fun(K, _V, Acc) -> [K | Acc] end, [], Data),
+	Values = maps:fold(fun(_K, V, Acc) -> [V | Acc] end, [], Data),
+	binary:replace(In, <<"{{" ++ [K | KeysRest] = Keys} ++ "}}">>, << [Value | ValueRest] = Values).
+
