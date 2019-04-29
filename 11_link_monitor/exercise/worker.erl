6cb58bf0051c1b558d94fe369240a55fb96acb7c
VBurak <vicctor.b.0711@gmail.com>
2019-04-29 13:54:30 +0300

    hometask of lesson 11 completed

--- 11_link_monitor/exercise/worker.erl
+++ 11_link_monitor/exercise/worker.erl
@@ -1,1 +1,21 @@
 -module(worker).
+-export([parser/2, parse_strings/1]).
+
+parser(File, ParentPid) ->
+    {ok, Bin} = file:read_file(File),
+    Strings = binary:split(Bin, <<"\n">>, [global]),
+    ParsedResult = parse_strings(Strings),
+    ParentPid ! {result, ParsedResult}.
+
+parse_strings(Strings) ->
+    lists:foldl(
+    fun
+        (<<>>, Acc) -> Acc;
+        (String, Acc) ->
+            [_Id, Name, BinaryQuantity, _Price]= binary:split(String, <<",">>, [global]),
+            IntegerQuantity = list_to_integer(binary_to_list(BinaryQuantity)),
+            Acc#{Name => IntegerQuantity}
+    end,
+    #{},
+    Strings).
+    
